fe8448bd706141149d422d919618287c16638849
Rafal R <rafalrogalski84@gmail.com>
2022-05-12 21:44:21 +0200

    Removing validation from Reservation & ReservationDao class and adding AllArgsConstructor to ReservationDao class

--- src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
+++ src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
@@ -1,11 +1,6 @@
 package com.sda.conference_room.model.dto;
 
-import com.sun.istack.NotNull;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -13,10 +8,9 @@ import java.time.LocalDateTime;
 @Setter
 @Builder(setterPrefix = "with")
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class ReservationDto {
 
-    @NotNull
-    @Length(min = 2, max = 20)
     private Long id;
     private LocalDateTime starting;
     private LocalDateTime ending;
