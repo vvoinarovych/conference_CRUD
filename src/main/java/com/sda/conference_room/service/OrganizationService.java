880751206c48f29bd5d1dec11849144126512ca4
Walerij <vvoinarovych@gmail.com>
2022-05-12 13:28:11 +0200

    defined OrganizationService methods

--- src/main/java/com/sda/conference_room/service/OrganizationService.java
+++ src/main/java/com/sda/conference_room/service/OrganizationService.java
@@ -1,4 +1,18 @@
 package com.sda.conference_room.service;
 
+import com.sda.conference_room.model.entity.Organization;
+
+import java.util.List;
+
 public interface OrganizationService {
+
+    Organization saveOrganization(Organization organization);
+
+    List<Organization> getALlOrganizations();
+
+    Organization getOrganizationById(Long id);
+
+    Organization updateOrganization(Long organizationId, Organization organization);
+
+    void deleteOrganizationById(Long id);
 }
