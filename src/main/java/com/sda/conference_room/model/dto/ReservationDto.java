d048bacf5119bfa83a552a7ac232b0d97c729abc
Rafal R <rafalrogalski84@gmail.com>
2022-05-12 17:39:16 +0200

    Updated Reservation Entity & Reservation DTO classes

--- src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
+++ src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
@@ -1,4 +1,24 @@
 package com.sda.conference_room.model.dto;
 
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder(setterPrefix = "with")
+@RequiredArgsConstructor
 public class ReservationDto {
+
+    @NotNull
+    @Length(min = 2, max = 20)
+    private Long id;
+    private LocalDateTime starting;
+    private LocalDateTime ending;
+
 }
--- src/main/java/com/sda/conference_room/model/entity/Reservation.java
+++ src/main/java/com/sda/conference_room/model/entity/Reservation.java
@@ -1,26 +1,37 @@
 package com.sda.conference_room.model.entity;
 
-
-import lombok.Data;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Builder(setterPrefix = "with")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "reservations")
+
 public class Reservation {
 
     @Id
+    @Column(name = "reservation_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Length(min = 2, max = 20)
     private Long id;
 
+    @NotNull
     private LocalDateTime starting;
 
+    @NotNull
     private LocalDateTime ending;
 
     @ManyToOne
     private ConferenceRoom conferenceRoom;
-
 
 
 }
