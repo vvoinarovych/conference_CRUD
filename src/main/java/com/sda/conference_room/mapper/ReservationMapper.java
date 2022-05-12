e600ec7d0ea4f14076d0ad9ca8698301a04a0227
Rafal R <rafalrogalski84@gmail.com>
2022-05-12 17:43:42 +0200

    Added Reservation service layer with ReservationService interface and its class implementation. Added Reservation Mapper class.

---
+++ src/main/java/com/sda/conference_room/mapper/ReservationMapper.java
@@ -0,0 +1,24 @@
+package com.sda.conference_room.mapper;
+
+import com.sda.conference_room.model.dto.ReservationDto;
+import com.sda.conference_room.model.entity.Reservation;
+
+public class ReservationMapper {
+
+    public static ReservationDto map(final Reservation reservation) {
+        return ReservationDto.builder()
+                .withId(reservation.getId())
+                .withStarting(reservation.getStarting())
+                .withEnding(reservation.getEnding())
+                .build();
+    }
+
+    public static Reservation map(final ReservationDto reservationDto) {
+        return Reservation.builder()
+                .withId(reservationDto.getId())
+                .withStarting(reservationDto.getStarting())
+                .withEnding(reservationDto.getEnding())
+                .build();
+
+    }
+}
