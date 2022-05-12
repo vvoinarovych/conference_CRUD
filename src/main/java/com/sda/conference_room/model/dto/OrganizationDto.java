c174db9c3e092cdae8004411f10f36eabbf0587a
igor <kolodziejigor@gmail.com>
2022-05-12 18:28:58 +0200

    @Min and @Max changed to @Size in OrganizationDto

--- src/main/java/com/sda/conference_room/model/dto/OrganizationDto.java
+++ src/main/java/com/sda/conference_room/model/dto/OrganizationDto.java
@@ -1,8 +1,12 @@
 package com.sda.conference_room.model.dto;
-import lombok.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Builder(setterPrefix = "with")
 @AllArgsConstructor
@@ -13,8 +17,7 @@ public class OrganizationDto {
     private Long id;
 
     @NotNull
-    @Min(2)
-    @Max(20)
+    @Size(min = 2, max = 20)
     private String name;
 
     private String password;
