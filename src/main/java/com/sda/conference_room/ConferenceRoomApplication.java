078a90891efe83872d3941944219988573da7d13
igor <kolodziejigor@gmail.com>
2022-05-12 11:14:44 +0200

    PR test

--- src/main/java/com/sda/conference_room/ConferenceRoomApplication.java
+++ src/main/java/com/sda/conference_room/ConferenceRoomApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConferenceRoomApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ConferenceRoomApplication.class, args);
     }
