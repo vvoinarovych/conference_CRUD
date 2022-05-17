c706c441525b8fe8686c19a81531ac365e4a552f
Rafal R <rafalrogalski84@gmail.com>
2022-05-17 11:36:25 +0200

    Updating Reservation and ReservationDto classes. Added new field for a reservation identifier. Added constraints and column names to fields. Appropriate changes in mapper and service classes.

--- src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
+++ src/main/java/com/sda/conference_room/model/dto/ReservationDto.java
@@ -2,6 +2,9 @@ package com.sda.conference_room.model.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,7 +15,15 @@ import java.time.LocalDateTime;
 public class ReservationDto {
 
     private Long id;
+
+    @NotNull
     private LocalDateTime starting;
+
+    @NotNull
     private LocalDateTime ending;
+
+    @NotBlank(message = "Reservation identifier must not be blank.")
+    @Size(min = 2, max = 20, message = "Reservation identifier must be between 2 and 20 characters long.")
+    private String reservationIdentifier;
 
 }
