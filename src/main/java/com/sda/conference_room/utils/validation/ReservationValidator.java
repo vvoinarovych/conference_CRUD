d18f33e2f9e63175a29d6646c143e046fc1a5dde
Walerij <vvoinarovych@gmail.com>
2022-05-28 23:47:00 +0200

    delete conferenceRoomFiltering

--- src/main/java/com/sda/conference_room/utils/validation/ReservationValidator.java
+++ src/main/java/com/sda/conference_room/utils/validation/ReservationValidator.java
@@ -17,9 +17,8 @@ public class ReservationValidator {
     private final ReservationRepository reservationRepository;
 
     public boolean isRoomAvailableInSpecificPeriod(ConferenceRoom conferenceRoom, LocalDateTime start, LocalDateTime end) {
-        List<Reservation> reservations = reservationRepository.findAll();
+        List<Reservation> reservations = reservationRepository.getAllByConferenceRoom(conferenceRoom);
         List<Reservation> reservationsForThatRoom = reservations.stream()
-                .filter(reservation -> reservation.getConferenceRoom().getName().equals(conferenceRoom.getName()))
                 .filter(reservation -> (reservation.getStarting().isBefore(end) || reservation.getStarting().isEqual(end))
                         && (reservation.getEnding().isAfter(start) || reservation.getEnding().isEqual(start)))
                 .collect(Collectors.toList());
